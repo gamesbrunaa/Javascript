8a528f43af4e64ad8eb6fe78a98fe3b60cd3e59c
gamesbrunaa <91560228+gamesbrunaa@users.noreply.github.com>
2022-02-21 15:33:55 -0300

    Novos exercicios .html .js

---
+++ Exercicios/aula09/ex008.js
@@ -0,0 +1,9 @@
+console.log('O console funcionou corretamente!')
+
+var  v = 50
+console.log('A velocidade do seu caroo é: ', v)
+if (v > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+} else {
+    console.log('Dirija sempre usando cinto de segurança!')
+}
---
+++ Exercicios/aula09/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'Brasil'
+if (pais == 'Brasil'){
+    console.log('você é brasileiro!')
+} else {
+    console.log('você é estrangeiro!')
+}
