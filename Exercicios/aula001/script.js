807264d68eab29f2109b3e1d0ddc45fd6c80383f
Bruna Veloso <91560228+gamesbrunaa@users.noreply.github.com>
2024-03-30 16:10:00 -0300

    aulas-revisao #1

---
+++ Exercicios/aula001/script.js
@@ -0,0 +1,60 @@
+console.log('bruna');
+
+var nome1 = 'bruna';
+let idade1 = 23;
+const possuiFaculdade1 = true; // const nao pode modificar
+var comidaFavorita;
+comidaFavorita = 'creme de galinha';
+
+var um, 
+    dois, 
+    tres, 
+    quatro, 
+    cinco;
+
+/* 
+var preco = 20;
+var qtd = 5
+var valorTotal = preco * qtd;
+*/
+
+var nome = 'André'; // String
+var idade = 28; // Number
+var possuiFaculdade = true; // Boolean
+var time; // Undefined
+var comida = null; // Null
+var simbolo = Symbol() // Symbol
+var novoObjeto = {} // Object
+
+var nome = 'André';
+console.log(typeof nome);
+// retorna string
+
+var sobrenome = 'Rafael';
+var nomeCompleto = nome + ' ' + sobrenome;
+
+var gols = 1000;
+var frase = 'Romário fez ' + gols + ' gols';
+
+'JavaScript é "super" fácil';
+"JavaScript é 'super' fácil";
+"JavaScript é \"super\" fácil";
+`JavaScript é "super" fácil"`;
+//"JavaScript é "super" fácil"; // Inválido
+
+var gols = 1000;
+var frase1 = 'Romário fez ' + gols + ' gols';
+var frase2 = `Romário fez ${gols} gols`; // Utilizando Template String
+
+var sobrenome = 'veloso';
+var idadade2 = '23';
+var nome = 'bruna';
+var nomeCompleto = nome + ' ' + sobrenome;
+var frase = "it's time!";
+
+console.log(typeof nome);
+
+
+
+
+
