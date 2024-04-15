1946539a596fa587d3200ad2cebc8ce920d38c8b
Bruna Veloso <91560228+gamesbrunaa@users.noreply.github.com>
2024-04-15 16:42:24 -0300

    mais exercicios

--- Exercicios/aula004/script.js
+++ Exercicios/aula004/script.js
@@ -70,4 +70,22 @@ var cachorro = {
 
 cachorro.ver(homem);
   
+// nomeie 3 propriedades ou métodos de strings
 
+var nome = 'bruna';
+console.log(nome.charAt());
+console.log(nome.length);
+console.log(nome.toLocaleUpperCase());
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector('.btn');
+btn.addEventListener;
+btn.appendChild;
+btn.innerHTML;
+btn.id;
+btn.localName;
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+//clipboard.js -> https://www.treinaweb.com.br/blog/clipboard-js-copiar-e-colar-com-javascript
